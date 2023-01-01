f91acac2d500d7f892eaaf4e3251b90a0c929bbd
janlingen <82512348+janlingen@users.noreply.github.com>
2023-01-01 22:21:37 +0100

    Add basic first fetch to DashboardService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './services/dashboard.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'springboot-actuator-dashboard';
+export class AppComponent implements OnInit {
+  
+  constructor(private dashboardService: DashboardService){}
+
+  ngOnInit(): void {
+    this.dashboardService.fetchHttpTraces();
+  }
 }
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Trace } from './trace.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DashboardService {
+  traces: Trace[] = [];
 
-  constructor() { }
+  private SERVER_URL = environment.serverUrl;
+
+  constructor(private httpClient: HttpClient) {}
+
+  public fetchHttpTraces() {
+    this.httpClient
+      .get(`${this.SERVER_URL}/httptrace`)
+      .subscribe((data: any) => {
+        console.log(data);
+      });
+  }
+
+  createAllTraces(data: any) {}
 }
