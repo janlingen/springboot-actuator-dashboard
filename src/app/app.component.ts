464da312c547ef620d21fba28632c3b21bc7bd94
janlingen <82512348+janlingen@users.noreply.github.com>
2023-01-02 21:07:28 +0100

    Implement new DashboardService metrics into html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { DashboardService } from './services/dashboard.service';
+import { Trace } from './services/trace.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  
-  constructor(private dashboardService: DashboardService){}
+  traces: Trace[] = [];
+  private tracesChanged: Subscription;
+  systemHealth: String = 'DOWN';
+  private systemHeatlhChanged: Subscription;
+  systemUptime: number = 0;
+  private systemUptimeChanged: Subscription;
+  systemSpace: number = 0;
+  private systemSpaceChanged: Subscription;
+  systemCpu: number = 0;
+  private systemCpuChanged: Subscription;
 
-  ngOnInit(): void {
-    this.dashboardService.fetchHttpTraces();
+  constructor(private dashboardService: DashboardService) {
+    this.tracesChanged = this.dashboardService.tracesChanged.subscribe(
+      (traces: Trace[]) => (this.traces = traces)
+    );
+    this.systemHeatlhChanged =
+      this.dashboardService.systemHealthChanged.subscribe(
+        (systemHeatlh: String) => (this.systemHealth = systemHeatlh)
+      );
+    this.systemUptimeChanged =
+      this.dashboardService.systemUptimeChanged.subscribe(
+        (systemUptime: number) => (this.systemUptime = systemUptime)
+      );
+    this.systemSpaceChanged =
+      this.dashboardService.systemSpaceChanged.subscribe(
+        (systemSpace: number) => (this.systemSpace = systemSpace)
+      );
+    this.systemCpuChanged = this.dashboardService.systemCpuChanged.subscribe(
+      (systemCpu: number) => (this.systemCpu = systemCpu)
+    );
+  }
+
+  ngOnInit(): void {}
+
+  public refreshPage() {
+    window.location.reload();
   }
 }
