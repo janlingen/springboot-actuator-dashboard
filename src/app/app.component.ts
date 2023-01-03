7ab2084372a81f65b043df3dc764092c39a60f81
janlingen <82512348+janlingen@users.noreply.github.com>
2023-01-03 22:40:55 +0100

    Implement bar chat with chart.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { DashboardService } from './services/dashboard.service';
 import { Trace } from './services/trace.model';
+import { Chart } from 'node_modules/chart.js/auto';
+import { R3TargetBinder } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +21,8 @@ export class AppComponent implements OnInit {
   private systemSpaceChanged: Subscription;
   systemCpu: number = 0;
   private systemCpuChanged: Subscription;
+  errorOccured: number[] = [0, 0, 0, 0];
+  private errorOccuredChanged: Subscription;
 
   constructor(private dashboardService: DashboardService) {
     this.tracesChanged = this.dashboardService.tracesChanged.subscribe(
@@ -39,9 +43,41 @@ export class AppComponent implements OnInit {
     this.systemCpuChanged = this.dashboardService.systemCpuChanged.subscribe(
       (systemCpu: number) => (this.systemCpu = systemCpu)
     );
+    this.errorOccuredChanged =
+      this.dashboardService.errorOccuredChanged.subscribe(
+        (errorOccured: number[]) => {
+          this.errorOccured = errorOccured;
+          this.createChart();
+        }
+      );
   }
 
   ngOnInit(): void {}
+
+  public createChart() {
+    new Chart('myChart', {
+      type: 'bar',
+      data: {
+        labels: ['200', '400', '404', '500'],
+        datasets: [
+          {
+            label: 'error incidents',
+            data: this.errorOccured,
+            backgroundColor: ['green', 'red', 'red', 'red'],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: { 
+            ticks: {
+              stepSize: 1
+            }
+          }
+        }
+      }
+    });
+  }
 
   public refreshPage() {
     window.location.reload();
